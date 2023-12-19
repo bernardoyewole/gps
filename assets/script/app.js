5fbd6cc9ace3ef54652fdc1c609f679306f3e2f3
Bernard <oyebenard@gmail.com>
2023-12-19 09:26:23 -0600

    Marker adjusts to real time location

--- assets/script/app.js
+++ assets/script/app.js
@@ -2,41 +2,63 @@
 
 import { onEvent, select, selectAll, create, print } from "./utils.js";
 
+let lat;
+let long;
+
+mapboxgl.accessToken = 'pk.eyJ1IjoieXV2cnhqc3IiLCJhIjoiY2xxM3g4a3NhMDE0bzJrbnZ6dGp6cmQwYSJ9.ko0Ddi2P09rnxx5TYkiSpQ';
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [-97.143173, 49.895313],
+    zoom: 16
+});
+
+
 function getLocation(position){    
     const {latitude, longitude} = position.coords;
-
+    lat = latitude;
+    long = longitude;
+    center(latitude, longitude);
     mapConfig(longitude, latitude);
 }
  
+function center(latitude, longitude) {
+    map.flyTo({
+      center: [longitude, latitude],
+      essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+      zoom: 16,
+    });
+}
+
 function errorHandler(){
-    mapConfig(-97.19267,49.81486);
+    console.log(error.message);
 }
  
 const options = {
     enableHighAccuracy: true,
-    maximumAge: 0
+    maximumAge: 0,
+    timeout: 5000
 };
  
-mapboxgl.accessToken = 'pk.eyJ1IjoieXV2cnhqc3IiLCJhIjoiY2xxM3g4a3NhMDE0bzJrbnZ6dGp6cmQwYSJ9.ko0Ddi2P09rnxx5TYkiSpQ';
-const marker1 = new mapboxgl.Marker({color: '#ff7342'})
+let currentMarker = null;
 
-function mapConfig(longitude, latitude){
-    const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v12',
-        center: [longitude,latitude],
-        zoom: 16
-    });
- 
+function mapConfig(longitude, latitude) {
+
     map.addControl(
         new MapboxDirections({
         accessToken: mapboxgl.accessToken
         }),
         'top-left'
     );
-    
-    marker1.setLngLat([longitude, latitude])
+
+    currentMarker ? currentMarker.remove() : '';
+
+    const marker = new mapboxgl.Marker({color: '#ff7342'})
+    .setLngLat([longitude, latitude])
     .addTo(map);
+
+    currentMarker = marker;
 }
 
 
